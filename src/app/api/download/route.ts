b16ba6d48c40f7a7bba420b7e4cf55dbab0ab445
jia <linme.jiayu@outlook.com>
2024-01-22 14:16:06 +0800

    ..

--- src/app/api/download/route.ts
+++ src/app/api/download/route.ts
@@ -5,7 +5,8 @@ import { headers } from "next/headers";
 export async function GET(req: Request, res: Response) {
     const fileContent = fs.readFileSync(
         // "/Users/jia/Pictures/壁纸/wallhaven-1k7v3v.jpg"
-        "/Users/jia/workFile/TourMind/tm-nft-mint/public/videos/2-1-2.mp4",
+        // "/Users/jia/workFile/TourMind/tm-nft-mint/public/videos/2-1-2.mp4",
+        "",
     );
     // res.writeHead(200, { "Content-Type": contentType });
     // res.end(content, "utf-8");
